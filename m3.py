4d7109f134fedfcea8292198a7e33fcb793363e2
Farooq Mahmud <f>
2020-03-24 10:39:43 -1000

    Added docstrings

--- m3.py
+++ m3.py
@@ -1,8 +1,25 @@
+"""
+    A module containing helpers to download and print words contained in documents.
+
+    Usage:
+        python m3.py <URL>
+"""
+
+
 import sys
 from urllib.request import urlopen
 
 
 def get_words(url):
+    """ Fetch a list of words from the specified URL.
+
+        Args:
+            url: The URL of a UTF-8 text document.
+
+        Returns:
+            A list of words from the document.
+    """
+
     try:
         book = urlopen(url)
         word_list = []
@@ -19,11 +36,23 @@ def get_words(url):
 
 
 def print_words(word_list):
+    """ Print a list of words, one word per line.
+
+            Args:
+                word_list: The word list.
+
+        """
     for word in word_list:
         print(word)
 
 
 def main(url):
+    """ Print a list of words from the specified URL.
+
+            Args:
+                url: The URL of a UTF-8 text document.
+
+        """
     words = get_words(url)
     print_words(words)
 
