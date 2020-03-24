8302ec0b8f45cb8bf50e83526c3a4897d3f7ce76
Farooq Mahmud <f>
2020-03-24 10:29:37 -1000

    Modularizing code

--- m3.py
+++ m3.py
@@ -1,15 +1,32 @@
+import sys
 from urllib.request import urlopen
 
-url = "http://www.gutenberg.org/files/100/100-0.txt"
-book = urlopen(url)
-word_list = []
 
-for line in book:
-    words = line.decode("utf8").split()
+def get_words(url):
+    try:
+        book = urlopen(url)
+        word_list = []
 
-    for word in words:
-        word_list.append(word)
+        for line in book:
+            words = line.decode("utf8").split()
 
-print(f"Ingested {len(word_list)} words.")
+            for word in words:
+                word_list.append(word)
 
-book.close()
+        return word_list
+    finally:
+        book.close()
+
+
+def print_words(word_list):
+    for word in word_list:
+        print(word)
+
+
+def main(url):
+    words = get_words(url)
+    print_words(words)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
